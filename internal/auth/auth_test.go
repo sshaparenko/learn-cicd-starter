1f00fb451f1be8338998c3a561944f4c260ddf96
sshaparenko <zshaparenkoz@gmail.com>
2024-09-16 19:36:04 +0300

    Adding test for auth package

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	var header http.Header = map[string][]string{}
+	header.Set("Authorization", "ApiKey gdjkslJAf345fogkDS34LgdsormvD")
+
+	got, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Test failed: %s", err.Error())
+	}
+	fmt.Println(got)
+	want := "gdjkslJAf345fogkDS34LgdsormvD"
+
+	if strings.Compare(got, want) != 0 {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+}
